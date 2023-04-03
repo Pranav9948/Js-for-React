25b14dbc5f4c5643d2c1baef7aecd6780686074f
Pranav <pranavat9948@gmail.com>
2023-04-03 15:49:25 +0530

    1st video completed

---
+++ index.js
@@ -0,0 +1,33 @@
+let player1="virat",
+player2="sachin",
+player3="dhoni";
+
+
+console.log(player1+player2+player3)
+
+
+const STADIUMS1='chinnaswamy',
+STADIUMS2='chepauk',
+STADIUMS3='wankhade';
+
+console.log(STADIUMS1,STADIUMS2,STADIUMS3)
+
+
+if(true){
+
+    var rcb="royal challengeres banglore"
+}
+
+
+console.log("1",rcb)
+
+
+function food(){
+
+    var morning='toasted bread'
+}
+console.log('2',morning)
+
+food()
+
+console.log('3',morning)
