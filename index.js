87f37c9e49ef7188d471a10e9f5b483497f1042e
Pranav <pranavat9948@gmail.com>
2023-04-03 16:52:53 +0530

    4rth video Or operator

--- index.js
+++ index.js
@@ -1,57 +1,79 @@
-let player1="virat",
-player2="sachin",
-player3="dhoni";
+// let player1="virat",
+// player2="sachin",
+// player3="dhoni";
 
 
-console.log(player1+player2+player3)
+// console.log(player1+player2+player3)
 
 
-const STADIUMS1='chinnaswamy',
-STADIUMS2='chepauk',
-STADIUMS3='wankhade';
+// const STADIUMS1='chinnaswamy',
+// STADIUMS2='chepauk',
+// STADIUMS3='wankhade';
 
-console.log(STADIUMS1,STADIUMS2,STADIUMS3)
-
-
-if(true){
-
-    var rcb="royal challengeres banglore"
-}
+// console.log(STADIUMS1,STADIUMS2,STADIUMS3)
 
 
-console.log("1",rcb)
+// if(true){
+
+//     var rcb="royal challengeres banglore"
+// }
 
 
-
+// console.log("1",rcb)
 
 
 
-const status='raining'
-let isMatchToday;
 
-if(status==="raining"){
 
-    isMatchToday="yes"
-}
 
-else{
+// const status='raining'
+// let isMatchToday;
+
+// if(status==="raining"){
+
+//     isMatchToday="yes"
+// }
+
+// else{
 
     
-    isMatchToday="No"
-}
+//     isMatchToday="No"
+// }
 
-console.log("3",isMatchToday)
-
-
-let climate='sun'
-let matchStatus
-
-climate==='sun' ?  console.log(" we  have a match today") :  console.log("  no we  have a match today") 
+// console.log("3",isMatchToday)
 
 
+// let climate='sun'
+// let matchStatus
 
-const team1Score='280',
-team2Score="134"
+// climate==='sun' ?  console.log(" we  have a match today") :  console.log("  no we  have a match today") 
 
-team1Score > team2Score ?  console.log('team 1 wins') : console.log('team 2 wins')
 
+
+// const team1Score='280',
+// team2Score="134"
+
+// team1Score > team2Score ?  console.log('team 1 wins') : console.log('team 2 wins')
+
+
+
+let playerFirstName='',
+playerMidName='',
+playerLastName='kohli';
+
+let nameInJersey=playerFirstName|| playerMidName|| playerLastName
+
+
+console.log('',nameInJersey)
+
+
+
+let sponsors=false
+ audiences=true;
+
+ const isMatchHappening= sponsors || audiences
+
+
+ isMatchHappening ? "yes match happen" : " no match "
+
+ console.log(isMatchHappening)
