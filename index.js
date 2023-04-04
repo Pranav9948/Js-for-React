8ea4cc4ded72b3ded81a0ee7c1bc163ee1ed84f6
Pranav <pranavat9948@gmail.com>
2023-04-04 11:49:29 +0530

    completed upto functions

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-// let player1="virat",
-// player2="sachin",
-// player3="dhoni";
+let player1="virat",
+player2="sachin",
+player3="dhoni";
 
 
-// console.log(player1+player2+player3)
+console.log(player1+player2+player3)
 
 
-// const STADIUMS1='chinnaswamy',
-// STADIUMS2='chepauk',
-// STADIUMS3='wankhade';
+const STADIUMS1='chinnaswamy',
+STADIUMS2='chepauk',
+STADIUMS3='wankhade';
 
-// console.log(STADIUMS1,STADIUMS2,STADIUMS3)
-
-
-// if(true){
-
-//     var rcb="royal challengeres banglore"
-// }
+console.log(STADIUMS1,STADIUMS2,STADIUMS3)
 
 
-// console.log("1",rcb)
+if(true){
+
+    var rcb="royal challengeres banglore"
+}
 
 
-
+console.log("1",rcb)
 
 
 
-// const status='raining'
-// let isMatchToday;
 
-// if(status==="raining"){
 
-//     isMatchToday="yes"
-// }
 
-// else{
+const status='raining'
+let isMatchToday;
+
+if(status==="raining"){
+
+    isMatchToday="yes"
+}
+
+else{
 
     
-//     isMatchToday="No"
-// }
+    isMatchToday="No"
+}
 
-// console.log("3",isMatchToday)
-
-
-// let climate='sun'
-// let matchStatus
-
-// climate==='sun' ?  console.log(" we  have a match today") :  console.log("  no we  have a match today") 
+console.log("3",isMatchToday)
 
 
+let climate='sun'
+let matchStatus
 
-// const team1Score='280',
-// team2Score="134"
+climate==='sun' ?  console.log(" we  have a match today") :  console.log("  no we  have a match today") 
 
-// team1Score > team2Score ?  console.log('team 1 wins') : console.log('team 2 wins')
+
+
+const team1Score='280',
+team2Score="134"
+
+team1Score > team2Score ?  console.log('team 1 wins') : console.log('team 2 wins')
 
 
 
@@ -77,3 +77,84 @@ let sponsors=false
  isMatchHappening ? "yes match happen" : " no match "
 
  console.log(isMatchHappening)
+
+
+
+
+// OR operator
+
+
+const umpires=true,
+      audiences=true,
+      sponsorss=false ;
+
+
+      let isMatchsToday;
+
+      isMatchToday=umpires && audiences && sponsors
+
+      isMatchToday ? console.log('yes today we are having a match') : console.log('No we dont having a match') 
+
+
+
+// NotOperator
+
+// seee index.html
+
+// Nullish operator
+
+
+let duckScore=0
+   centuryScore=100;
+
+   let playerScore=duckScore ?? centuryScore
+
+   console.log('43',playerScore)
+
+// javascript functions
+
+
+function  showMyFavCricketPlayer(x="virat"){
+
+    console.log(`my fav cricket player is ${x}`)
+}
+
+
+
+
+let favPlayer;
+
+showMyFavCricketPlayer(favPlayer)
+
+
+// see index.html
+
+
+// fn expression and declaration
+
+
+// fn declaration
+
+
+function showPlayerName(){
+
+    console.log(`player is sachin`)
+}
+
+
+const hello=showPlayerName
+
+hello()
+
+
+
+fn expression
+
+
+const showme=function(){
+
+
+    console.log("this is")
+}
+
+showme()
